02d29d06df6ff1362bd3f9e52bff1ae16658508b
Suh Youri <suhyouri@gmail.com>
2022-07-14 22:12:50 +0900

    vercel deploy

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-import express from "express";
-const app = express();
-import path from "path";
-const __dirname = path.resolve(); 
-import { createServer } from "http"; 
+// //1. node server.js
+// import express from "express";
+// const app = express();
+// import path from "path";
+// const __dirname = path.resolve(); 
+// import { createServer } from "http"; 
 
+// const port = 5000;
+
+// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+// app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+// })
+
+// const httpServer = createServer(app); 
+
+// httpServer.listen(port, () => {
+//   console.log(`it's alive on http://localhost:${port}`);
+// });
+
+// 2. Vercel Deployment
+import express from "express";
+import path from "path";
+const __dirname = path.resolve();
+const app = express();
 const port = 5000;
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use(express.static("public"));
+//index.js
+// app.get("/*", (req, res) => {
+//   res.sendFile("index.html", { root: path.join(__dirname, "public") });
+// });
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
-})
+  res.sendFile(path.resolve(__dirname, "frontend/static/", "index.html"));
+});
 
-const httpServer = createServer(app); 
+// index.js
+// module.exports = app;
 
-httpServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`it's alive on http://localhost:${port}`);
 });
