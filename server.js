b813b072eb2c5db05f0917e6c0adf019fbb1fe8f
Suh Youri <suhyouri@gmail.com>
2022-07-14 16:48:36 +0900

    deploy try -01

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 const app = express();
 import path from "path";
 const __dirname = path.resolve(); 
-const port = 3000;
+import { createServer } from "http"; 
+
+const port = 5000;
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
@@ -10,7 +12,6 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 })
 
-import { createServer } from "http";  
 const httpServer = createServer(app); 
 
 httpServer.listen(port, () => {
