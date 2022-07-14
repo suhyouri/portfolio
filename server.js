0e4af1f84f903ab4a8701c289bae6c7474aa81b5
Suh Youri <suhyouri@gmail.com>
2022-07-14 22:20:03 +0900

    see

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
 // //1. node server.js
-// import express from "express";
-// const app = express();
-// import path from "path";
-// const __dirname = path.resolve(); 
-// import { createServer } from "http"; 
-
-// const port = 5000;
-
-// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
-
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
-// })
-
-// const httpServer = createServer(app); 
-
-// httpServer.listen(port, () => {
-//   console.log(`it's alive on http://localhost:${port}`);
-// });
-
-// 2. Vercel Deployment
 import express from "express";
-import path from "path";
-const __dirname = path.resolve();
 const app = express();
+import path from "path";
+const __dirname = path.resolve(); 
+import { createServer } from "http"; 
+
 const port = 5000;
 
-app.use(express.static("public"));
-//index.js
-// app.get("/*", (req, res) => {
-//   res.sendFile("index.html", { root: path.join(__dirname, "public") });
-// });
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/static/", "index.html"));
-});
+    res.sendFile(path.resolve(__dirname, "frontend/static", "index.html"));
+})
 
-// index.js
-// module.exports = app;
+const httpServer = createServer(app); 
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`it's alive on http://localhost:${port}`);
 });
+
+// 2. Vercel Deployment
+// import express from "express";
+// import path from "path";
+// const __dirname = path.resolve();
+// const app = express();
+// const port = 5000;
+
+// app.use(express.static("public"));
+// //index.js
+// // app.get("/*", (req, res) => {
+// //   res.sendFile("index.html", { root: path.join(__dirname, "public") });
+// // });
+
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend/static/", "index.html"));
+// });
+
+// // index.js
+// // module.exports = app;
+
+// app.listen(port, () => {
+//   console.log(`it's alive on http://localhost:${port}`);
+// });
